4e67a4e92af2955aef18f178d0ed6b94d25a83bd
wortjanin <wortjanin@yandex.ru>
2015-10-24 15:56:40 +0300

    first commit

---
+++ f.ps1
@@ -0,0 +1,38 @@
+﻿
+[string[]] $strs = @() 
+
+$stringCount = 512
+$chunk_size = 512
+for( $j=0; $j -lt 3; $j++) {
+    [byte[]] $bytes = @()
+    for( $i=0; $i -lt $chunk_size; $i++) {
+	[int] $mod = (Get-Random) % 255
+	if( $mod -eq 0 ) {
+	  $mod=95
+	}
+	$bytes+=@($mod)
+    }
+
+    $strs += @([System.Text.Encoding]::ASCII.GetString($bytes))
+}
+
+$invocation = (Get-Variable MyInvocation).Value
+$directorypath = Split-Path $invocation.MyCommand.Path
+
+$nFiles = 32
+for( $j=0; $j -lt $nFiles; $j++) {
+
+    $filepath = $directorypath + '\files\test.bin' + $j
+
+    $nStrings = 1024
+    for( $i=0; $i -lt $nStrings; $i++) {
+	[byte] $byte = 255
+	
+	[int] $mod = (Get-Random) % $strs.Count
+
+	out-file -filepath $filepath -inputobject $strs[$mod] -append 
+
+    }
+ 
+    echo "file $($j+1) out of $nFiles"
+}
---
+++ f2.ps1
@@ -0,0 +1,60 @@
+﻿Function GetFreeDiskSpace([string]$DriveLetter, [string]$Measurement)
+{
+
+  $Drives=Get-WmiObject -Class Win32_LogicalDisk
+
+  foreach($Drive in $Drives)
+  {
+
+    If ($Drive.Name -eq $DriveLetter) 
+    {
+        return $Drive.FreeSpace/“1$Measurement“
+    }
+
+  }
+
+}
+
+
+$C_space = GetFreeDiskSpace “C:” 
+
+$D_space = GetFreeDiskSpace “D:” 
+
+$rnd = Get-Random
+
+write-host “Freespace for disk C: = $C_space, and for D: = $D_space” 
+
+
+write-host "$rnd"
+
+
+
+$invocation = (Get-Variable MyInvocation).Value
+$directorypath = Split-Path $invocation.MyCommand.Path
+
+$filepath = $directorypath + '\files\test.bin0'
+
+[int] $numToCopy = $($D_space / (Get-Item $filepath).length)
+
+write-host "$numToCopy files to copy"
+
+
+del $($directorypath + '\fileCopies\*')
+
+
+$nFiles = 32
+$nToCopy = $numToCopy
+for( $j=0; $j -lt $nToCopy; $j++) {
+
+    [int] $mod = (Get-Random) % $nFiles
+
+    $filepath = $directorypath + '\files\test.bin' + $mod
+
+    $copyPath = $directorypath + '\fileCopies\copy' + $j + '_from' + $($mod + 1)
+
+    copy $filepath $copyPath 
+
+    if($j % 1000 -eq 0){
+    	write-host "$j files copied out of $nToCopy"
+    }
+}
